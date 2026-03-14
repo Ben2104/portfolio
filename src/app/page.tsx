a75cab6c49f75b692a431548f22b6e7f00b4b126
Ben2104 <dohoangkhoi341@gmail.com>
2026-03-14 12:28:31 -0700

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { PortfolioPage } from "@/components/portfolio/portfolio-page";
-
+import { Analytics } from "@vercel/analytics/next"
 export default function Page() {
-  return <PortfolioPage />;
+  return (
+    <>
+      <PortfolioPage />
+      <Analytics />
+    </>
+  );
 }
 
